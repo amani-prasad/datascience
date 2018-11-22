97774195d60b4b432ad26c5939514f8f8561a78c
amani <amani.prasad@revealitsolutions.com>
2018-11-22 10:44:16 +0530

    Create udareset.sh
    Add file

---
+++ udareset.sh
@@ -0,0 +1,18 @@
+tempv="agnt"
+udadpath=$(ps -ef | grep discagnt| grep -o -P '(?<= -d ).*(?=agnt)')
+udadpath="$udadpath$tempv"
+oldudid=$(grep 'UD_UNIQUE_ID=.*$' ${udadpath}/aioptionrc | cut -c14- )
+echo 'old UDA ID:'${oldudid}
+udabasedirreveal=$(grep 'BASEDIR=.*$' ${udadpath}/aioptionrc | cut -c9- )
+${udabasedirreveal}/bin/discagnt --newuniqueid
+newudid=$(grep 'UD_UNIQUE_ID=.*$' ${udadpath}/aioptionrc | cut -c14- )
+echo 'new UDA ID:'${newudid}
+if [ "${newudid}" == "${oldudid}" ]
+then
+echo Failed
+else echo Resetted
+fi
+/opt/HP/Discovery/bin/discagnt --newuniqueid
+echo Resetted
+/usr/lpp/HP/ Discovery/bin/discagnt --newuniqueid
+echo Resetted
